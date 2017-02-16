30fc175342454bb1e2dcaa324bd154a111b9ff8f
ManishPandey1508 <manish.123kumarpandey@gmail.com>
2017-02-16 15:12:35 -0800

    Final Version

--- CMPE_273/socket-mon.py
+++ CMPE_273/socket-mon.py
@@ -5,9 +5,11 @@
 import psutil
 import pyformat
 import pandas as pd
+import subprocess
+import BaseHTTPServer
+import sys
 
 
-print'{:{align}{width}} {:{align}{width}} {:{align}{width}} {:{align}{width}} '.format("PID", "LADDR", "RADDR", "STATUS", align='^', width='20')
 d = pd.DataFrame()
 for conn in psutil.net_connections(kind='tcp'):
     laddr = ""
@@ -15,11 +17,18 @@ for conn in psutil.net_connections(kind='tcp'):
     dictExample = {}
     if(conn.raddr and conn.laddr):
         temp = pd.DataFrame(
-            {'PID': conn.pid, 'LADDR': conn.laddr, 'RADDR': conn.raddr, 'STATUS': conn.status})
+            {'PID': int(conn.pid), 'LADDR': conn.laddr, 'RADDR': conn.raddr, 'STATUS': conn.status})
         d = pd.concat([d, temp])
+d['freq'] = d.groupby('PID')['PID'].transform('count')
+d = d.sort_values(by=["freq", "PID"], ascending=[False, False])
+header = ["PID", "LADDR", "RADDR", "STATUS"]
+print d.to_csv(sys.stdout, columns=header, sep=",", index=False)
 
 
-grouped = d.groupby('PID')
-
-for i in range(0, len(d)):
-    print'{:{align}{width}} {:{align}{width}} {:{align}{width}}{:{align}{width}} '.format(d.iloc[i]['PID'], d.iloc[i]['LADDR'], d.iloc[i]['RADDR'], d.iloc[i]['STATUS'], align='^', width='20')
+# print d.columns
+# print'{:{align}{width}} {:{align}{width}} {:{align}{width}}
+# {:{align}{width}} '.format("PID", "LADDR", "RADDR", "STATUS", align='^',
+# width='30')
+# for i in range(0, len(d)):
+#   print'{:{align}{width}} {:{align}{width}} {:{align}{width}}{:{align}{width}} '.format(d.iloc[i]['PID'], d.iloc[i]['LADDR'], d.iloc[i]['RADDR'], d.iloc[i]['STATUS'], align='^', width='30')
+#
